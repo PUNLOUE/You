0add07cd08349308615aff7ede482494d2f5dc77
PUNLOUE <pichpunloue3@gmail.com>
2024-02-27 15:01:16 +0700

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,32 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/about', function () {
+    return view('about');
 });
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/posts', function () {
+    $posts = [
+        ['title' => 'Post 1', 'content' => 'Content for service 1'],
+        ['title' => 'Post 2', 'content' => 'Content for service 2'],
+        ['title' => 'Post 3', 'content' => 'Content for service 3'],
+    ];
+    return view('service.post', ['posts' => $posts]);
+});
+Route::get('/products/{id}', function ($id) {
+    $product = [
+        'id' => $id,
+        'name' => 'Product ' . $id,
+        'description' => 'Description for product ' . $id,
+        'price' => 100,
+    ];
+    return view('service.product', compact('product'));
+});
+
